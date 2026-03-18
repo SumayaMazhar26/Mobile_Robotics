674b3562232715e320d94dcb4a93b103fec2ab21
Sumaya Mazhar <sumayamazhar006@gmail.com>
2026-03-18 16:29:23 +0500

    Initial package setup and square pattern working

---
+++ my_turtle_package/my_turtle_package/move_turtle.py
@@ -0,0 +1,34 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+import time
+
+class VelocityPublisher(Node):
+    def __init__(self):
+        super().__init__('velocity_publisher')
+        self.publisher_ = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
+        self.timer_period = 0.5  # seconds
+        self.timer = self.create_timer(self.timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        msg = Twist()
+        for i in range(4):  # Move in a square pattern
+            msg.linear.x = 2.0
+            msg.angular.z = 0.0
+            self.publisher_.publish(msg)
+            time.sleep(2)
+            
+            msg.linear.x = 0.0
+            msg.angular.z = 1.57 # Turn 90 degrees
+            self.publisher_.publish(msg)
+            time.sleep(1)
+
+def main(args=None):
+    rclpy.init(args=args)
+    velocity_publisher = VelocityPublisher()
+    rclpy.spin(velocity_publisher)
+    velocity_publisher.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
